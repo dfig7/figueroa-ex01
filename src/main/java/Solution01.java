893551dec581441ce8b021183a894bcde44afd39
diego <diegofigueroa0519@gmail.com>
2021-09-05 23:07:19 -0400

    Initial commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+/*
+Print: What is your name?
+save name into String variable name
+print: Hello, 'name', nice to meet you!
+ */
+public class Solution01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("What is your name? ");
+        String name = sc.nextLine();
+        System.out.printf("Hello, %s, nice to meet you!", name);
+    }
+
+}
